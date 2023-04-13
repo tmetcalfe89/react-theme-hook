964f023fedad15259a22d7a2b8787ac137b43bd9
Tim M <82600217+tmetcalfe89@users.noreply.github.com>
2023-04-13 22:06:19 +0000

    oh right, the other import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useThemeDetector = () => {
   const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
